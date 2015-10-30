f497c44a11110d7eb193b0512bf9b54ee13c4e3a
Phil Czek <phil.czek@gmail.com>
2015-10-30 14:27:07 +0100

    added title

--- js/cb.js
+++ js/cb.js
@@ -1,11 +1,19 @@
 function updateBoxSize(){
   $(".box").height($(".box").width());
-  console.log($(".box").width() + " = "+ $(".box").height())
+}
+
+function updateTitleSize(){
+  $("#title").width($(".container").width())
+  console.log("do shit");
+
 }
 
 function CollectiveBerlin( jQuery ) {
   updateBoxSize();
+  updateTitleSize()
   $(window).resize(updateBoxSize);
+  $(window).resize(updateTitleSize);
+
 }
 
 $( document ).ready( CollectiveBerlin );
